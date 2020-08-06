c0a4919918c70a80d79652543a89d074a81663bf
albertobolsi <66950864+albertobolsi@users.noreply.github.com>
2020-08-06 16:08:45 +0200

    Eliminated useless file "borrador.py"

--- borrador.py
+++
@@ -1,8 +0,0 @@
-play_again = "nope"
-affirmative = ["Yes","yes","Y","Yep","yep"]
-negative = ["No","no","N","Nope","nope"]
-aff_answer = any(element in play_again for element in affirmative)
-neg_answer = any(element in play_again for element in negative)
-
-print(aff_answer)
-print(neg_answer)
